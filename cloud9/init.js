d5c027982ec931ce698f1d30b62aa81212ab901d
Tim Malone <tdmalone@gmail.com>
2018-03-14 13:29:12 +1100

    New dir for cloud9 plus add missing files

---
+++ cloud9/init.js
@@ -0,0 +1,8 @@
+// You can access plugins via the 'services' global variable
+/*global services, plugin*/
+
+// to load plugins use
+// services.pluginManager.loadPackage([
+//     "https://<user>.github.io/<project>/build/package.<name>.js",
+//     "~/.c9/plugins/<name>/package.json",
+// ]);
